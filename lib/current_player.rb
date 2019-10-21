ca75e2bacde0ed5bea2230a5b8ce8cb909bd5b92
Barak Saidoff <barak.saidoff@gmail.com>
2019-10-21 21:43:20 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,10 +9,10 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count.even?
+  turns = turn_count(board)
+  if turns % 2 == 0
     return "X"
-  else 
+  else
     return "O"
   end
-  
 end
